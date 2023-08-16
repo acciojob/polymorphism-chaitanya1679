316507dfa19b01c7984911a61f8bc800eec3c63a
Chaitanya Ramekar <ramekarc@gmail.com>
2023-08-16 16:37:11 +0530

    Submitting 1st ans

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
+    public static class Product{
 
+        public int product(int x, int y) {
+            return x*y;
+        }
+
+        public int product(int x, int y, int z){
+            return x*y*z;
+        }
+
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
+    Product p= new Product();
+    int ans1=p.product(4,5);
+    int ans2=p.product(4,5,6);
+    double ans3=p.product(4.5,5.6);
 }
